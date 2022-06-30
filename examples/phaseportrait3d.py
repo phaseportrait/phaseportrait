c607af7b8380e2e09155a52c9206289420c9a937
Unai Lería Fortea <unicelularuni15@gmail.com>
2022-06-30 14:09:07 +0200

    Add 2 examples of PhasePortrait3D

---
+++ examples/phaseportrait3d.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+
+import phaseportrait
+from phaseportrait.streamlines import *
+
+
+def dF(x,y,z):
+    return -x, -y, -z
+
+p1 = phaseportrait.PhasePortrait3D(dF, [-3, 3])
+plt.show()
+
+
+
+def dF(x,y,z, *, w=1):
+    return -y, x, -z + w
+
+
+p2 = phaseportrait.PhasePortrait3D(dF, [-3, 3], MeshDim=8, maxLen=2500, deltat=0.05)
+
+p2.add_slider('w')
+
+p2.plot(color='viridis', grid=False)
+plt.show()
